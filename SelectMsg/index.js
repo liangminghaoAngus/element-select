7e4a92b9c1d77dae92f1c5683851f9026418d043
liangminghaoAngus <34882512+liangminghaoAngus@users.noreply.github.com>
2019-11-01 15:04:23 +0800

    Update index.js
    做了取消的返回动作信息

--- SelectMsg/index.js
+++ SelectMsg/index.js
@@ -11,6 +11,7 @@ Vue.prototype.$select = createSelectBox
  * 
  * @param {*} option 继承 $msg
  * @param {*} option.value  select 绑定的值
+ * @param {string} option.placeholder  select 绑定的 placeholder 填充内容
  * @param {Array} option.list  select 的可选值
  * @returns {Promise} 
  */
@@ -19,6 +20,7 @@ function createSelectBox(option) {
   let value = option.value || '';
   return vm.$msgbox({
       ...option,
+      showCancelButton: true,
       beforeClose: async (action, instance, done) => {
         if (action !== "confirm") {
           done()
@@ -41,10 +43,21 @@ function createSelectBox(option) {
       })
     })
     .then(v => {
-      console.log(v);
+      // console.log(v);
       return Promise.resolve({
         action: v,
         value
       })
+    })
+    .catch(e => {
+      // console.log(e);
+      if (e === 'cancel') {
+        return Promise.resolve({
+          action: e,
+          value
+        })
+      } else {
+        return Promise.reject('error')
+      }
     })
 }
